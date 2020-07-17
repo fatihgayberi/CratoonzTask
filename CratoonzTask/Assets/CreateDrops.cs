caa669b0adef0781e1fcf9b3badd63057628dc3e
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-17 15:07:29 +0300

    Create Drops and Table

---
+++ CratoonzTask/Assets/CreateDrops.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDrops : MonoBehaviour
+{
+    public GameObject [] Drops; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                CreateTable(x, y);
+            }
+        }
+    }
+
+    int CreateRandDropNum()
+    {
+        int rand = Random.Range(0, Drops.Length);
+
+        return rand;
+    }
+
+    void CreateTable(int x, int y)
+    {
+        GameObject drop = GameObject.Instantiate(Drops[CreateRandDropNum()], new Vector2(x, y), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
