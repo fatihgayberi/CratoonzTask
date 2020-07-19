8ff587bc04e5a5651e05d1e8b4a2a70ba8dd4c25
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-19 04:00:39 +0300

    Swipe

--- CratoonzTask/Assets/Scripts/CreateDrops.cs
+++ CratoonzTask/Assets/Scripts/CreateDrops.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CreateDrops : MonoBehaviour
 {
-    static int height = 8, width = 8; // tablonun yuksekligini ve genisligini tutar
-    public GameObject[] Drops;
+    public static int width = 8, height = 8; // tablonun yuksekligini ve genisligini tutar
+    public GameObject[] drops;
+    public GameObject[,] allDrops = new GameObject[height, width]; //
     int[,] dropArray = new int[height, width];
     // Start is called before the first frame update
     void Start()
@@ -15,25 +16,61 @@ public class CreateDrops : MonoBehaviour
 
     void CreateGame(int n, int m)
     {
-        for (int i = 0; i < n; i += 1) // satir
+        for (int i = 0; i < n; i++) //satir
         {
-            for (int j = 0; j < m; j++) // sutun
+            for (int j = 0; j < m; j++) //sutun
             {
-                dropArray[i, j] = Random.Range(0, Drops.Length);
+                dropArray[i, j] = Random.Range(0, drops.Length);
             }
         }
-               
+
+        ColumnControl(n, m); // sutun control
+        LineControl(n, m); // satir control
 
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                GameObject.Instantiate(Drops[dropArray[i, j]], new Vector2(i, j), Quaternion.identity);
+                allDrops[i, j] = GameObject.Instantiate(drops[dropArray[i, j]], new Vector2(i, j), Quaternion.identity);
             }
         }
     }
 
-    
+    void ColumnControl(int n, int m) // sutun control
+    {
+        int randNum;
+
+        for (int i = 0; i < n; i++) //satir
+        {
+            for (int j = 0; j < m - 2; j++) //sutun
+            {
+                while (dropArray[i, j] == dropArray[i, j + 1] && dropArray[i, j + 1] == dropArray[i, j + 2]) // sutunlari duzenler
+                {
+                    randNum = Random.Range(0, 3);
+                    dropArray[i, j + 2] = Random.Range(0, drops.Length);
+                }
+            }
+        }
+    }
+
+    void LineControl(int n, int m) // satir control
+    {
+        int randNum;
+
+        for (int i = 0; i < n - 2; i += 2) //satir
+        {
+            for (int j = 0; j < m; j++) //sutun
+            {
+                while (dropArray[i + 1, j] == dropArray[i + 2, j]) // satirlari duzenler
+                {
+                    randNum = Random.Range(0, 3);
+                    dropArray[i + 2, j] = Random.Range(0, drops.Length);
+                }
+            }
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
---
+++ CratoonzTask/Assets/Scripts/SwipeDrop.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDrop : MonoBehaviour
+{
+    public int firstX, firstY, finalX, finalY;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    public Vector2 touchObject;
+    public CreateDrops table;
+    private GameObject emptyDrop;
+    public float tangent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch();
+    }
+
+    void Touch()
+    {
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch touch = Input.GetTouch(0);
+                touchObject = Camera.main.ScreenToWorldPoint(touch.position);
+                firstTouchPosition = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
+                
+                if (0 < touchObject.x && touchObject.x < CreateDrops.width && 0 < touchObject.y && touchObject.y < CreateDrops.height)
+                {
+                    firstX = (int)(touchObject.x + 0.5f);
+                    firstY = (int)(touchObject.y + 0.5f);
+                }
+                
+                Debug.Log("First: " + firstX + ", " + firstY);
+                //Debug.Log("FirstPosition: " + firstTouchPosition.x);
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Touch touch = Input.GetTouch(0);
+                touchObject = Camera.main.ScreenToWorldPoint(touch.position);
+                finalTouchPosition = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
+                TangentCalculator();
+
+                if (0 < touchObject.x && touchObject.x < CreateDrops.width && 0 < touchObject.y && touchObject.y < CreateDrops.height)
+                {
+                    finalX = (int)(touchObject.x + 0.5f);
+                    finalY = (int)(touchObject.y + 0.5f);
+                    
+                    Swipe();
+                }
+                
+                Debug.Log("First: " + finalX + ", " + finalY);
+                //Debug.Log("FinalPosition: " + finalTouchPosition.x);
+                
+            }
+        }
+    }
+
+    void TangentCalculator()
+    {
+        tangent = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        //Debug.Log("Anguler: " + tangent);
+    }
+
+    void Swipe()
+    {
+        if (tangent < 45f && tangent > -45f)
+        {
+            Debug.Log("hi");
+            //Debug.Log("nam: " + table.allDrops[finalX, finalY].name + "\n" + finalX + ", " + finalY);
+            //emptyDrop = table.allDrops[finalX, finalY];
+            //table.allDrops[finalX, finalY].transform.position = new Vector2(table.allDrops[firstX, firstY].transform.position.x, table.allDrops[firstX, firstY].transform.position.y);
+            //table.allDrops[finalX, finalY] = table.allDrops[firstX, firstY];
+            //table.allDrops[firstX, firstY].transform.position = new Vector2(finalX, finalY);
+            //table.allDrops[firstX, firstY] = emptyDrop;
+        }
+    }
+
+
+}
